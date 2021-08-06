61f317d56bb2f9ccee6fff4dc9f496f425921a37
pedromaironi <pedrom_t@hotmail.com>
2021-08-06 06:18:23 +0200

    [Categories] Adding categories controller v0.2

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,38 +1,38 @@
 module.exports = (app) => {
-    const users = require('../controllers/user.controller.js');
+  const users = require("../controllers/user.controller.js");
 
-    // Create a new User
-    app.post('/users', users.create);
+  // Create a new User
+  app.post("/users", users.create);
 
-    // Retrieve all Users
-    app.get('/users', users.findAll);
+  // Retrieve all Users
+  app.get("/users", users.findAll);
 
-    // Retrieve a single User with userId
-    app.get('/users/:userId', users.findOne);
+  // Retrieve a single User with userId
+  app.get("/users/:userId", users.findOne);
 
-    // Update a User with userId
-    app.put('/users/:userId', users.update);
+  // Update a User with userId
+  app.put("/users/:userId", users.update);
 
-    // Delete a User with userId
-    app.delete('/users/:userId', users.delete);
+  // Delete a User with userId
+  app.delete("/users/:userId", users.delete);
 
-    app.post('/signIn', users.signIn);
+  app.post("/signIn", users.signIn);
 
-    app.post('signUp', users.signUp);
+  app.post("signUp", users.signUp);
+};
 
-    
+module.exports = (app) => {
+  const Categories = require("../controllers/Categories.controller.js");
 
-    const Categories = require('../controllers/Categories.controller.js');
-    
-    // Retrieve all Categoriess
-    app.get('/categories', Categories.findAll);
+  // Retrieve all Categoriess
+  app.get("/categories", Categories.findAll);
 
-    // Retrieve a single Categories with CategoriesId
-    app.get('/categories/:categoriesId', Categories.findOne);
+  // Retrieve a single Categories with CategoriesId
+  app.get("/categories/:categoriesId", Categories.findOne);
 
-    // Update a Categories with CategoriesId
-    app.put('/categories/:categoriesId', Categories.update);
+  // Update a Categories with CategoriesId
+  app.put("/categories/:categoriesId", Categories.update);
 
-    // Delete a Categories with CategoriesId
-    app.delete('/categories/:categoriesId', Categories.delete);
-}
+  // Delete a Categories with CategoriesId
+  app.delete("/categories/:categoriesId", Categories.delete);
+};
