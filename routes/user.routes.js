5c47b8325842979fea8c4b7feff98d90701bd139
pedromaironi <pedrom_t@hotmail.com>
2021-08-06 06:10:10 +0200

    [Categories] Adding categories controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -19,4 +19,20 @@ module.exports = (app) => {
     app.post('/signIn', users.signIn);
 
     app.post('signUp', users.signUp);
+
+    
+
+    const Categories = require('../controllers/Categories.controller.js');
+    
+    // Retrieve all Categoriess
+    app.get('/categories', Categories.findAll);
+
+    // Retrieve a single Categories with CategoriesId
+    app.get('/categories/:categoriesId', Categories.findOne);
+
+    // Update a Categories with CategoriesId
+    app.put('/categories/:categoriesId', Categories.update);
+
+    // Delete a Categories with CategoriesId
+    app.delete('/categories/:categoriesId', Categories.delete);
 }
